47d563808d144e6f556b228df2bb4ed1fbaaaf86
Victor Tereschenko <sibvic@gmail.com>
2019-07-22 15:48:57 +0600

    Updated signaler

--- snippets/signaler.cs
+++ snippets/signaler.cs
@@ -1,50 +1,69 @@
+// Signaler 1.1
+void InitAlert()
+{
+    AlertPlaySound = false;
+    AlertSoundName = "";
+    AlertSendEmail = false;
+    AlertEmail = "";
+    AlertDebug = true;
+}
+
+
+[NinjaScriptProperty]
+[Display(Name="Play sound", Order = 1000, GroupName = "Alert")]
+public bool AlertPlaySound
+{ get; set; }
+
+[NinjaScriptProperty]
+[Display(Name="Sound", Order = 1001, GroupName = "Alert")]
+public string AlertSoundName
+{ get; set; }
+
+[NinjaScriptProperty]
+[Display(Name="Send email", Order = 1002, GroupName = "Alert")]
+public bool AlertSendEmail
+{ get; set; }
+
+[NinjaScriptProperty]
+[Display(Name="Email", Order = 1003, GroupName = "Alert")]
+public string AlertEmail
+{ get; set; }
+
+[NinjaScriptProperty]
+[Display(Name="Print debug output", Order = 1100, GroupName = "Alert")]
+public bool AlertDebug
+{ get; set; }
+
 class Signaler
 {
-    void InitAlert()
+    bool _alertPlaySound;
+    string _alertSoundName;
+    bool _alertSendEmail;
+    string _alertEmail;
+    bool _alertDebug;
+    Indicator _indicator;
+
+    public void Init(Indicator indicator, bool alertPlaySound, string alertSoundName, bool alertSendEmail, string alertEmail, bool alertDebug)
     {
-        AlertPlaySound = false;
-        AlertSoundName = "";
-        AlertSendEmail = false;
-        AlertEmail = "";
-        AlertDebug = true;
+        _alertPlaySound = alertPlaySound;
+        _alertSoundName = alertSoundName;
+        _alertSendEmail = alertSendEmail;
+        _alertEmail = alertEmail;
+        _alertDebug = alertDebug;
     }
+
     DateTime _alertLastDate;
-    void SendSignal(string message, DateTime dt)
+    public void SendSignal(string message, DateTime dt)
     {
         if (_alertLastDate == dt)
             return;
         _alertLastDate = dt;
-        if (AlertPlaySound)
-            PlaySound(AlertSoundName);
+        if (_alertPlaySound)
+            _indicator.PlaySound(_alertSoundName);
 
-        if (AlertSendEmail)
-            SendMail(AlertEmail, message, message);
-        if (AlertDebug)
-            Print(message);
+        if (_alertSendEmail)
+            _indicator.SendMail(_alertEmail, message, message);
+        if (_alertDebug)
+            _indicator.Print(message);
     }
-
-    [NinjaScriptProperty]
-    [Display(Name="Play sound", Order = 1000, GroupName = "Alert")]
-    public bool AlertPlaySound
-    { get; set; }
-
-    [NinjaScriptProperty]
-    [Display(Name="Sound", Order = 1001, GroupName = "Alert")]
-    public string AlertSoundName
-    { get; set; }
-
-    [NinjaScriptProperty]
-    [Display(Name="Send email", Order = 1002, GroupName = "Alert")]
-    public bool AlertSendEmail
-    { get; set; }
-
-    [NinjaScriptProperty]
-    [Display(Name="Email", Order = 1003, GroupName = "Alert")]
-    public string AlertEmail
-    { get; set; }
-
-    [NinjaScriptProperty]
-    [Display(Name="Print debug output", Order = 1100, GroupName = "Alert")]
-    public bool AlertDebug
-    { get; set; }
 }
